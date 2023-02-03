01900e6a0c31f468e5aa31d829fe138519b6d719
tochi <tochi@aguuu.com>
2023-02-03 15:23:42 +0900

    Initial commit.

---
+++ WorldOriginSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorldOriginSample
+//
+//  Created by Tomoyuki Tochihira on 2023/02/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
